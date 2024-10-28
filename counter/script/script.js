b6d30c3a40cc5f8f2e1bf63dde85b4eb1ae0ea38
Reeze0811 <2301212@s.asojuku.ac.jp>
2024-10-28 11:07:58 +0900

    新規作成
    index.htmlとscript.jsの作成

--- counter/script/script.js
+++ counter/script/script.js
@@ -3,6 +3,25 @@ new Vue({
     el: '#app',
     data() {
         return {
+            count: 1,
+            num: 0
         };
+    },
+    methods:{
+        double(){
+            this.count = this.count * 2;
+        },
+        half(){
+            this.count = this.count / 2;
+        },
+        reset(){
+            this.count = 1;
+        },
+        add(){
+            this.count = this.count + this.num;
+        },
+        pull(){
+            this.count = this.count - this.num;
+        }
     }
 });
